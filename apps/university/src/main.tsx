d92d192c3a6ee46da732d559c65af3bd1a92e681
PieAI <piecaisky@gmail.com>
2026-08-26 06:14:31 +0800

    refactor(ui): move source sheet styles beside component

--- apps/university/src/main.tsx
+++ apps/university/src/main.tsx
@@ -26,6 +26,7 @@ import "@pieai/university-ui/entry/entry-page.css";
 import "@pieai/university-ui/entry/style-sample.css";
 import "@pieai/university-ui/evidence/evidence.css";
 import "@pieai/university-ui/evidence/evidence-inline-source.css";
+import "@pieai/university-ui/evidence/source-sheet.css";
 import "@pieai/university-ui/feedback/feedback-note.css";
 import { FeedbackNote } from "@pieai/university-ui/feedback/FeedbackNote.js";
 import "@pieai/university-ui/favourites/favourites.css";
